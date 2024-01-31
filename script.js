391346f7b89f01bf12a8ec9db7a5eb0845118d7d
João Bruske <joaobruske@yahoo.com.br>
2024-01-31 18:48:06 -0300

    Criadas as funções isPalindrome e arrayMaxMinx
    Criada a função isPalindrome que verifica se uma sctring é um palíndromo.
    Criada a função arrayMaxMin que retorna o menor e o maior número de um array.

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
-
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+function isPalindrome(str) {
+    let strSemEspacos = "";
+    for (let index = 0; index < str.length; index++) {
+      if (str[index] !== " ") {
+        strSemEspacos += str[index];
+      }
+    }
+  
+    let strEmMinusculo = strSemEspacos.toLowerCase();
+  
+    let strInvertido = "";
+    for (let index = strEmMinusculo.length - 1; index >= 0; index--) {
+      strInvertido += strEmMinusculo[index];
+    }
+  
+    if (strEmMinusculo === strInvertido) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  function arrayMaxMin(arr) {
+    let maiorNumero = arr[0];
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] > maiorNumero) {
+        maiorNumero = arr[index];
+      }
+    }
+  
+    let menorNumero = arr[0];
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] < menorNumero) {
+        menorNumero = arr[index];
+      }
+    }
+  
+    let arrayMinMax = [menorNumero, maiorNumero];
+    return arrayMinMax;
+  }
